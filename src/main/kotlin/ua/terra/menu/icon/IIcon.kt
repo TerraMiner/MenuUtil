8453af6cb5d1455454c93b5b19fb748c32013248
sasha <sasha0koreckij@gmail.com>
2023-12-03 02:09:57 +0100

    Page pattern fix Cloneable icons

--- src/main/kotlin/ua/terra/menu/icon/IIcon.kt
+++ src/main/kotlin/ua/terra/menu/icon/IIcon.kt
@@ -1,8 +1,11 @@
 package ua.terra.menu.icon
 
 import org.bukkit.inventory.ItemStack
+import ua.terra.menu.utils.menuIcon
 
 interface IIcon {
     var slot: Int
     var stack: ItemStack
+
+    fun clone() = menuIcon(slot, stack.clone())
 }
--- src/main/kotlin/ua/terra/menu/icon/functional/IFuncIcon.kt
+++ src/main/kotlin/ua/terra/menu/icon/functional/IFuncIcon.kt
@@ -11,7 +11,7 @@ interface IFuncIcon : IIcon {
     var accessor: IconAccessor
     val iconUpdaters: MutableList<IconUpdater>
 
-    fun clone() = menuIcon(slot, stack.clone()) builder@{
+    override fun clone() = menuIcon(slot, stack.clone()) builder@{
         clicks.addAll(this@IFuncIcon.clicks)
         iconUpdaters.addAll(this@IFuncIcon.iconUpdaters.map {
             IconUpdater(this, it.delay, it.period, it.backTicking, it.action)
